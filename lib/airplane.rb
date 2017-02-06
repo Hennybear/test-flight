db58e9a91eb4bf0327de51a853fa68e9f76a0563
HENNY <guerrahenny@gmail.com>
2017-02-06 14:38:18 -0800

    Completed- Part One: Create an Airplane Part Two: Make the Airplane do things Part Three: Refactoring Tests Extra Challenge: Keep track of Airplane's fuel status

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -1,2 +1,53 @@
 class Airplane
+	attr_reader :type, :wing_loading, :horse_power
+
+	def initialize(type, wing_loading, horse_power, fuel = 100)
+		@type, @wing_loading, @horsepower = type, wing_loading, horse_power
+		@airplane_state = "off"
+		@fuel = fuel
+	end
+
+	def start
+		if @fuel < 10
+			"Not enough fuel to start engine"
+		else
+			if @airplane_state == "on"
+				"airplane already started"
+			else
+				@airplane_state = "on"
+				@fuel -= 5
+				"airplane started"
+			end
+		end
+	end
+
+	def takeoff
+		if @fuel < 40
+			"Not enough fuel to takeoff"
+		else
+			if @airplane_state == "on"
+				@airplane_state = "flying"
+				@fuel -= 30
+	      "airplane launched"
+	    else
+	    	"airplane not started, please start"
+	    end
+  	end
+	end
+
+	def land
+		if @fuel < 15 
+			"Not enough fuel to land"
+		else
+			if @airplane_state == "off"
+				"airplane not started, please start"
+			elsif @airplane_state == "on"
+				"airplane already on the ground"
+			elsif @airplane_state == "flying"
+				@fuel -= 5
+				"airplane landed"
+			end
+		end
+	end
+
 end
--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -1,15 +1,89 @@
 require_relative "../../lib/airplane"
 
 describe Airplane do
+	let(:my_plane) { Airplane.new("cesna", 10, 150) }
+
   describe "#initialization" do
+  	it 'has a type of Cesna' do 
+  		expect(my_plane.type).to eq("cesna")
+  	end
   end
 
   describe "#start" do
-  end
+  	it 'starts the plane if the plane is off' do 
+  		expect(my_plane.start).to eq("airplane started")
+  	end
 
-  describe "#land" do
+  	it 'tells us that the plane was already started' do 
+  		my_plane.start
+  		expect(my_plane.start).to eq("airplane already started") 
+  	end
   end
 
   describe "#takeoff" do
+  	it 'returns that the "airplane launched"' do 
+   		my_plane.start
+  		expect(my_plane.takeoff).to eq("airplane launched") 	
+  	end
+
+  	it 'can only takeoff if the plane was already started' do 
+  		expect(my_plane.takeoff).to eq("airplane not started, please start")   		
+  	end
+
+  	it 'can only takeoff if the plane was already started' do 
+  		my_plane.start
+  		expect(my_plane.takeoff).to eq("airplane launched")
+  	end
   end
+
+  describe "#land" do
+  	it 'cannot land if the airplane is not started' do 
+  		expect(my_plane.land).to eq("airplane not started, please start")
+  	end
+
+		it 'cannot land if the aiplane is already on the ground' do 
+  		my_plane.start
+  		expect(my_plane.land).to eq("airplane already on the ground")
+  	end
+
+ 		it 'can only land if the airplane is already in the air' do 
+  		my_plane.start
+  		my_plane.takeoff
+  		expect(my_plane.land).to eq("airplane landed")
+  	end
+  end
+
+  describe "#fuel" do 
+  	it 'cannot start if the fuel level is below 10 gallons' do 
+  		my_plane = Airplane.new("cesna", 10, 150, 5)
+  		expect(my_plane.start).to eq("Not enough fuel to start engine")
+  	end
+
+  	it 'cannot takeoff if the fuel level is below 50 gallons' do 
+  		my_plane = Airplane.new("cesna", 10, 150, 39)
+  		my_plane.start
+  		expect(my_plane.takeoff).to eq("Not enough fuel to takeoff")
+  	end
+
+  	it 'cannot land if the fuel level is below 10 gallons' do 
+  		my_plane = Airplane.new("cesna", 10, 150, 45)
+  		my_plane.start
+  		my_plane.takeoff
+  		expect(my_plane.land).to eq("Not enough fuel to land")
+  	end
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
